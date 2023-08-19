c09039843e033f76da2cfbcf53db14df7fa49de0
Zachary Honey <zhoney466@gmail.com>
2023-08-18 19:04:04 -0700

    Trying to get setMessage to work all together

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,5 +1,6 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { SET_INFO_MESSAGE } from './action-types'
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
@@ -18,7 +19,16 @@ function selectedAnswer(state = initialSelectedAnswerState, action) {
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch(action.type) {
+    case SET_INFO_MESSAGE:
+      return {
+        ...state,
+        message: [...state, action.payload]
+      }
+      default:
+        return state
+
+  }
 }
 
 const initialFormState = {
