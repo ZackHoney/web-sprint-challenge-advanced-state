70073a3df37873ac9ce2b5a2b6295dbf04c6c782
Zachary Honey <zhoney466@gmail.com>
2023-08-20 20:13:23 -0700

    imported files into Quiz.js.

--- frontend/components/Quiz.js
+++ frontend/components/Quiz.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { selectAnswer, setQuiz, setMessage } from '../state/action-creators'
 
-export default function Quiz(props) {
+function Quiz(props) {
   return (
     <div id="wrapper">
       {
@@ -32,3 +34,9 @@ export default function Quiz(props) {
     </div>
   )
 }
+
+const mapStateToProps = state => {
+
+}
+
+export default connect(mapStateToProps, {})(Quiz)
